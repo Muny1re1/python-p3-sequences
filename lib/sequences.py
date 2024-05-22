bdc14775915577323d9e439502957b6c48758f1d
Muny1re1 <munyiriandy@gmail.com>
2024-05-22 14:05:40 +0300

    labDone

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    zer = 0
+    one = 1
+    fibonacci_list = []
+
+    if length == 0:
+        print(fibonacci_list)
+    elif length == 1:
+        fibonacci_list.append(zer)
+        print(fibonacci_list)
+    elif length == 2:
+        fibonacci_list.append(zer)
+        fibonacci_list.append(one)
+        print(fibonacci_list)
+    else:
+        fibonacci_list.append(zer)
+        fibonacci_list.append(one)
+
+        for i in range(2, length):
+            nv = zer + one
+            fibonacci_list.append(nv)
+            zer = one
+            one = nv
+
+        print(fibonacci_list)
+
+print_fibonacci(10)
+            
+    
+    
